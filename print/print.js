496d8fbe212789c313364814c2060b111ff18e1a
Mario Botsch <botsch@techfak.uni-bielefeld.de>
2019-11-24 01:11:46 +0100

    print plugin

---
+++ print/print.js
@@ -0,0 +1,113 @@
+"use strict";
+
+var RevealPrint = (function(){
+
+    /* this function is called inbetween just before Reveal's setupPDF.
+     * We setup the minimum height of slide elements to Reveal's page height.
+     */
+    function setHeight()
+    {
+        var height = Reveal.getConfig().height;
+		Reveal.getSlides().forEach( function( slide ) {
+            slide.style.minHeight = height + "px";
+        });
+    }
+
+
+    // pandoc puts footers into a <p> element, which
+    // makes positioning w.r.t. slide bottom difficult.
+    // hence we remove these p-elements and put the
+    // footers as children of the slide element
+    function fixFooters()
+    {
+		Reveal.getSlides().forEach( function( slide ) {
+            var footers = slide.getElementsByClassName('footer');
+            for (var i=0; i<footers.length; i++)
+            {
+                var footer = footers[i];
+                var parent = footer.parentElement;
+                if (parent.nodeName == "P")
+                {
+                    slide.appendChild(footer);
+                    if (parent.childNodes.length == 0) {
+                        parent.parentElement.removeChild(parent);
+                    }
+                }
+            }
+        });
+    }
+
+
+    /* check whether the current slide is too tall and print error in that case */
+    function checkHeight()
+    {
+        var configHeight  = Reveal.getConfig().height;
+        var slideHeight   = Reveal.getCurrentSlide().clientHeight;
+
+        if (slideHeight > configHeight)
+        {
+            console.error("slide " + slideNumber() + " is " + (slideHeight-configHeight) + "px too high");
+        }
+    }
+
+
+    /* return string with current slide number */
+    function slideNumber()
+    {
+        var idx = Reveal.getIndices();
+        return idx.v>0 ? "(h:" + (idx.h+1) + ", v:" + (idx.v+1) + ")" : idx.h+1;
+    }
+
+
+    /* load iframes that are marked with class 'pdf'.
+     * used to include simple Javascript demos in PDF
+     */
+    function loadIframes()
+    {
+        for (var e of document.querySelectorAll('.reveal section iframe.pdf'))
+        {
+            if (e.hasAttribute("data-src"))
+            {
+                e.src = e.getAttribute("data-src");
+            }
+        }
+    }
+
+
+    /* remove controls from videos, since they mess up printing
+     */
+    function printVideos()
+    {
+        for (var vid of document.getElementsByTagName("video")) 
+        {
+            vid.removeAttribute("controls");
+            vid.removeAttribute("autoplay");
+            vid.removeAttribute("data-autoplay");
+        }
+    }
+
+
+	return {
+		init: function() { 
+            return new Promise( function(resolve) {
+                Reveal.addEventListener( 'slidechanged', checkHeight );
+                Reveal.addEventListener( 'ready', setHeight  );
+                Reveal.addEventListener( 'ready', fixFooters );
+   
+                /* are we exporting a PDF? */
+                var pdf = !!window.location.search.match(/print-pdf/gi);
+                if (pdf)
+                {
+                    loadIframes();
+                    printVideos();
+                }
+
+                resolve();
+            });
+        }
+    }
+
+})();
+
+Reveal.registerPlugin( 'print', RevealPrint );
+
